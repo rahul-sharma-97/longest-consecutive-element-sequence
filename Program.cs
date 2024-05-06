4a5005678cb5b7ff32360911f2786e247fd4180e
rahul-sharma-97 <169043816+rahul-sharma-97@users.noreply.github.com>
2024-05-06 15:49:59 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int[] numbers = [2, 3, 101, 102, 103, 104, 101, 102, 103, 104, 105, 106, 200];
+
+int length = 1;
+int currLength = 1;
+
+for (var i = 0; i < numbers.Length - 1; i++)
+{
+    if (numbers[i] + 1 == numbers[i+1])
+    {
+        currLength++;
+    }
+    else
+    {
+        if (currLength > length)
+        {
+            length = currLength;
+        }
+        currLength = 1;
+    }
+}
+
+if (currLength > length)
+{
+    length = currLength;
+}
+
+Console.WriteLine(length);
+
